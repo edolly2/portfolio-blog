928563d5f20a0e9c818b155a50266a72aaebd261
dollyllama <edolly2@protonmail.com>
2022-06-20 23:49:56 -0500

    "Posts"

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,7 +1,20 @@
 import "./Posts.css";
 
+import Post from "./Post";
+
 const Posts = () => {
-  return <div className="posts">Posts</div>;
+  return (
+    <div className="posts">
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  );
 };
 
 export default Posts;
